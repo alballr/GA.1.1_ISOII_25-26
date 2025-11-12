c0c54b45b88918c0ebdeb95e570e289df9ce984c
aliciavo <Alicia.Villasenor@alu.uclm.es>
2025-11-12 16:13:47 +0100

    Added client maven project

---
+++ library-manager-client/src/main/java/es/uclm/esi/iso2/axx/library_manager_client/App.java
@@ -0,0 +1,7 @@
+package es.uclm.esi.iso2.axx.library_manager_client;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ library-manager-client/src/test/java/es/uclm/esi/iso2/axx/library_manager_client/AppTest.java
@@ -0,0 +1,13 @@
+package es.uclm.esi.iso2.axx.library_manager_client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
