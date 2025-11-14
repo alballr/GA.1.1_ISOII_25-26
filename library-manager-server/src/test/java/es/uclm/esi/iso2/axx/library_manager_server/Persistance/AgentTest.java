45293840d4ba999b27159a03c300ce8777dd4fb5
luciasc04 <146214675+luciasc04@users.noreply.github.com>
2025-11-14 22:29:52 +0100

    v 0.0.5
    iteration 3 testing version 0.0.5
    The testing corresponding to the iteration 3 of the server done

---
+++ library-manager-server/src/test/java/es/uclm/esi/iso2/axx/library_manager_server/Persistance/AgentTest.java
@@ -0,0 +1,42 @@
+package Persistance;
+
+import org.junit.Test;
+
+public class AgentTest {
+
+    @Test
+    public void testGetAgent() {
+        // Test para getAgent
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    @Test
+    public void testInsert() {
+        // Test para insert
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    @Test
+    public void testSelect() {
+        // Test para select
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    @Test
+    public void testUpdate() {
+        // Test para update
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    @Test
+    public void testDelete() {
+        // Test para delete
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    @Test
+    public void testConnect() {
+        // Test para connect
+        throw new RuntimeException("Not yet implemented");
+    }
+}}
