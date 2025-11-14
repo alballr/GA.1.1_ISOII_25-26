229a4654490d6acebf806d5ee337005337114fd8
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-11-14 22:01:29 +0100

    Create ServerAPITest.java

---
+++ library-manager-server/src/test/java/es/uclm/esi/iso2/axx/library_manager_server/ServerAPITest.java
@@ -0,0 +1,12 @@
+package es.uclm.esi.iso2.axx.library_manager_server;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServerAPITest {
+    
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
