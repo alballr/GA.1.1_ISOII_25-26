6bde7f5c2b6636290e005d8370c8914d90a0f04b
luciasc04 <146214675+luciasc04@users.noreply.github.com>
2025-11-14 21:07:09 +0100

    v.0.0.5
    iteration 3 implementation v 0.0.5

---
+++ library-manager-client/src/test/java/es/uclm/esi/iso2/axx/library_manager_client/Domain/LoanControllerTest.java
@@ -0,0 +1,18 @@
+package Domain;
+
+import org.junit.Test;
+
+public class LoanControllerTest {
+
+    @Test
+    public void testProcessLoan() {
+        // Test para processLoan
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    @Test
+    public void testProcessReturn() {
+        // Test para processReturn
+        throw new RuntimeException("Not yet implemented");
+    }
+}
