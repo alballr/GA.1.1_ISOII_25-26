93da0ee3538279e641bf74801ece3e14b6326785
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-11-14 22:03:23 +0100

    Update AppTest.java

--- library-manager-server/src/test/java/es/uclm/esi/iso2/axx/library_manager_server/AppTest.java
+++ library-manager-server/src/test/java/es/uclm/esi/iso2/axx/library_manager_server/AppTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppTest {
     
   @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
-  }
+  public void shouldRunMainWithoutErrors() {
+        assertDoesNotThrow(() -> App.main(new String[]{}));
+    }
 }
