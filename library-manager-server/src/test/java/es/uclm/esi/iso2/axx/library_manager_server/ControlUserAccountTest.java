e8416e709b724eaa2f54322009fb259d04b21cfc
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-11-14 22:08:28 +0100

    Update ControlUserAccountTest.java

--- library-manager-server/src/test/java/es/uclm/esi/iso2/axx/library_manager_server/ControlUserAccountTest.java
+++ library-manager-server/src/test/java/es/uclm/esi/iso2/axx/library_manager_server/ControlUserAccountTest.java
@@ -2,11 +2,13 @@ package es.uclm.esi.iso2.axx.library_manager_server;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import Control.Control_UserAccounts;
 
 public class ControlUserAccountTest {
     
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+        Control_UserAccounts control = new Control_UserAccounts();
+        assertNotNull(control);
     }
 }
