eca71d325822b97dad8bdc9fb532d593ca74c286
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-11-14 22:09:07 +0100

    Update ServerAPITest.java

--- library-manager-server/src/test/java/es/uclm/esi/iso2/axx/library_manager_server/ServerAPITest.java
+++ library-manager-server/src/test/java/es/uclm/esi/iso2/axx/library_manager_server/ServerAPITest.java
@@ -2,11 +2,13 @@ package es.uclm.esi.iso2.axx.library_manager_server;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import Interface.ServerAPI;
 
 public class ServerAPITest {
     
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+        ServerAPI api = new ServerAPI();
+        assertNotNull(api);
     }
 }
