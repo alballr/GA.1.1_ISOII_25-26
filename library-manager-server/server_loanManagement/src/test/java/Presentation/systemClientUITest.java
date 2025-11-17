235bc128b535e7e037f2923d419a01b69ba383f2
luciasc04 <lucia@Lucia.localdomain>
2025-11-17 19:25:40 +0100

    maven project in the server ,new modules

---
+++ library-manager-server/server_loanManagement/src/test/java/Presentation/systemClientUITest.java
@@ -0,0 +1,24 @@
+package Presentation;
+
+import org.junit.Test;
+
+public class systemClientUITest {
+
+    @Test
+    public void testReadAllLoans() {
+        // Test para readAllLoans
+        //throw new RuntimeException("Not yet implemented");
+    }
+
+    @Test
+    public void testReadSpesificLoan() {
+        // Test para readSpesificLoan
+        //throw new RuntimeException("Not yet implemented");
+    }
+
+    @Test
+    public void testUpdateLoanStatus() {
+        // Test para updateLoanStatus
+        //throw new RuntimeException("Not yet implemented");
+    }
+}
