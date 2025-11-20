fd407c1a5562d7663356031227d5c6899ddce800
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-11-20 23:27:15 +0100

    it4 Implementation v0.0.4

---
+++ library-manager-server/server_reservationManagement/src/main/java/Persistence/Agent.java
@@ -0,0 +1,19 @@
+package Persistence;
+
+public class Agent {
+
+    public boolean create(Object o) {
+        // TODO - implement Agent.create
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean update(Object o) {
+        // TODO - implement Agent.update
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean delete(Object o) {
+        // TODO - implement Agent.delete
+        throw new UnsupportedOperationException();
+    }
+}
