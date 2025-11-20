57cd2172bc7b9adaceee49612827c1eecdeaf8b3
aliciavo <Alicia.Villasenor@alu.uclm.es>
2025-11-20 17:20:08 +0100

    it5 Integration v.0.1.4

--- library-manager-client/client-bibliographicCatalog/src/main/java/Presentacion/ClientUI.java
+++ library-manager-client/client-bibliographicCatalog/src/main/java/Presentacion/ClientUI.java
@@ -1,7 +1,34 @@
 package Presentation.Client;
 
-public class ClientUI {
+import java.util.List;
 
+public class ClientUI {
+	
+	private ControlBrowsing controlBrowsing;
+
+	/**
+	 * 
+	 * @param control
+	 */
+	public ClientUI(ControlBrowsing control) {
+		// TODO - implement ClientUI.ClientUI
+		throw new UnsupportedOperationException();
+	}
+
+	public void requestNextPage() {
+		// TODO - implement ClientUI.requestNextPage
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param results
+	 */
+	private void displayResults(List results) {
+		// TODO - implement ClientUI.displayResults
+		throw new UnsupportedOperationException();
+	}
+	
 	public void createBibligraphicRecord() {
 		// TODO - implement ClientUI.createBibligraphicRecord
 		throw new UnsupportedOperationException();
