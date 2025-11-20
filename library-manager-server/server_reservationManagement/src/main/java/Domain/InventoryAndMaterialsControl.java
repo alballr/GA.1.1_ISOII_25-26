9d3ad24e3f3eef0db74dcadcf8f18ae4a8796368
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-11-20 23:21:23 +0100

    it4 Implementation v0.0.4

--- library-manager-server/server_reservationManagement/src/main/java/Domain/InventoryAndMaterialsControl.java
+++ library-manager-server/server_reservationManagement/src/main/java/Domain/InventoryAndMaterialsControl.java
@@ -1,1 +1,18 @@
+public class InventoryAndMaterialsControl {
 
+	public void addPhysicalMaterial() {
+		// TODO - implement InventoryAndMaterialsControl.addPhysicalMaterial
+		throw new UnsupportedOperationException();
+	}
+
+	public void updatePhysicalMaterial() {
+		// TODO - implement InventoryAndMaterialsControl.updatePhysicalMaterial
+		throw new UnsupportedOperationException();
+	}
+
+	public void deletePhysicalMaterial() {
+		// TODO - implement InventoryAndMaterialsControl.deletePhysicalMaterial
+		throw new UnsupportedOperationException();
+	}
+
+}
