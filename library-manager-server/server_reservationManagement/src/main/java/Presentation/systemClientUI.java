ec4bd0cd3fa7b72f835a57c96defd4cfcc3a490d
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-11-20 23:28:20 +0100

    it4 Implementation v0.0.4

---
+++ library-manager-server/server_reservationManagement/src/main/java/Presentation/systemClientUI.java
@@ -0,0 +1,19 @@
+package Presentation;
+
+public class systemClientUI {
+
+    public void addPhysicalMaterial() {
+        // TODO - implement systemClientUI.addPhysicalMaterial
+        throw new UnsupportedOperationException();
+    }
+
+    public void updatePhysicalMaterial() {
+        // TODO - implement systemClientUI.updatePhysicalMaterial
+        throw new UnsupportedOperationException();
+    }
+
+    public void deletePhysicalMaterial() {
+        // TODO - implement systemClientUI.deletePhysicalMaterial
+        throw new UnsupportedOperationException();
+    }
+}
