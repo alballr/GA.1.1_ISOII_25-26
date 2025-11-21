4885d7f0412e674508b5d586175e4d9205278d33
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-11-21 19:47:52 +0100

    it4 Testing v0.0.5

--- library-manager-server/server_reservationManagement/src/test/java/Persistance/AgentTest.java
+++ library-manager-server/server_reservationManagement/src/test/java/Persistance/AgentTest.java
@@ -1,42 +1,12 @@
-package Persistance;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class AgentTest {
 
     @Test
-    public void testGetAgent() {
-        // Test para getAgent
-        //throw new RuntimeException("Not yet implemented");
+    public void test() {
+        fail("Not yet implemented");
     }
 
-    @Test
-    public void testInsert() {
-        // Test para insert
-        //throw new RuntimeException("Not yet implemented");
-    }
-
-    @Test
-    public void testSelect() {
-        // Test para select
-       // throw new RuntimeException("Not yet implemented");
-    }
-
-    @Test
-    public void testUpdate() {
-        // Test para update
-        //throw new RuntimeException("Not yet implemented");
-    }
-
-    @Test
-    public void testDelete() {
-        // Test para delete
-        //throw new RuntimeException("Not yet implemented");
-    }
-
-    @Test
-    public void testConnect() {
-        // Test para connect
-        //throw new RuntimeException("Not yet implemented");
-    }
 }
