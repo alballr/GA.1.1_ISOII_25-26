f7aa68032c2b85967e317f59ac6e492bf3e384bf
irem-batigun <Irem.Batigun1@alu.uclm.es>
2025-11-27 23:09:45 +0100

    it8 Implementation v.0.0.4

---
+++ it8_implementation_client/Persistence/Agent.java
@@ -0,0 +1,44 @@
+package Persistence;
+
+import Domain.*;
+
+public class Agent {
+
+	public Connection connection() {
+		// TODO - implement Agent.connection
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param a
+	 */
+	public void saveRoleAssignment(RoleAssignment a) {
+		// TODO - implement Agent.saveRoleAssignment
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param a
+	 */
+	public void updateRoleAssignment(RoleAssignment a) {
+		// TODO - implement Agent.updateRoleAssignment
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param userId
+	 */
+	public Vector<RoleAssignment> fetchRoles(String userId) {
+		// TODO - implement Agent.fetchRoles
+		throw new UnsupportedOperationException();
+	}
+
+	public Vector<Role> fetchAllRoles() {
+		// TODO - implement Agent.fetchAllRoles
+		throw new UnsupportedOperationException();
+	}
+
+}
