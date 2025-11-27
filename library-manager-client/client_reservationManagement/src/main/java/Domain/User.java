865a90ed8a4f950bcad90d50e25d928375f95e39
HIBA EL FARISSI <Hiba.Elfarissi@alu.uclm.es>
2025-11-27 20:12:00 +0100

    it7 Implementation v 0.1.4

---
+++ library-manager-client/client_reservationManagement/src/main/java/Domain/User.java
@@ -0,0 +1,20 @@
+package Domain;
+
+public class User {
+
+	Loan makes;
+	private String userID;
+	private String name;
+	private String email;
+
+	public List<Reservation> getReservations() {
+		// TODO - implement User.getReservations
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Loan> getLoans() {
+		// TODO - implement User.getLoans
+		throw new UnsupportedOperationException();
+	}
+
+}
