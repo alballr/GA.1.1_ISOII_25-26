cddd38db1785429b31c817a4de717c6f009bb610
irem-batigun <Irem.Batigun1@alu.uclm.es>
2025-11-28 22:07:23 +0100

    it8 Test v.0.0.5

---
+++ library-manager-server/server_systemManagement/src/test/java/Domain/RolePermissionTest.java
@@ -0,0 +1,14 @@
+package Domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RolePermissionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ library-manager-server/server_systemManagement/src/test/java/Presentation/SystemClientUITest.java
@@ -0,0 +1,14 @@
+package Presentation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SystemClientUITest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
