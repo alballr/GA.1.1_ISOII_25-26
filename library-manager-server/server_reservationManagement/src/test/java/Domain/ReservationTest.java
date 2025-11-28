092eaf2361584b23b067ed2b3c8413e76897ee81
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-11-28 10:49:00 +0100

    it7 Testing v0.1.5

---
+++ library-manager-server/server_reservationManagement/src/test/java/Domain/ReservationTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReservationTest {
+
+    @Test
+    public void testReserve() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCancel() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testExpire() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsActive() {
+        fail("Not yet implemented");
+    }
+}
