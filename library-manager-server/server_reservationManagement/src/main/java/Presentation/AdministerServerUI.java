f18e6cba3889c2d91ba86a9961a21c23da4b3dee
Alba Llorente Ramirez <alba.llorente@alu.uclm.es>
2025-11-28 17:45:21 +0100

    Revert "Revert "Server reservation management""

---
+++ library-manager-server/server_reservationManagement/src/main/java/Presentation/AdministerServerUI.java
@@ -0,0 +1,20 @@
+package Presentation;
+
+public class AdministerServerUI {
+
+    public void displayReservations() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void displayRenewals() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void processReservationRequest() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void processRenewalRequest() {
+        throw new UnsupportedOperationException();
+    }
+}
