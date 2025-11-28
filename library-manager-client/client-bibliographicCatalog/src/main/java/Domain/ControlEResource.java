bcaa2cf9d4e9f34e07b9086440a22e379cbd0641
Álvaro de Torres Barón <163144126+AlvaroTorresUCLM@users.noreply.github.com>
2025-11-28 17:56:52 +0100

    Domain class fix added
    Lo siento gente, añadí mal la clase. Voy a volver a hacerlo desde github bien y borro lo otro.

---
+++ library-manager-client/client-bibliographicCatalog/src/main/java/Domain/ControlEResource.java
@@ -0,0 +1,20 @@
+package Domain;
+
+public class ControlEResource {
+
+	public void getResource() {
+		// TODO - implement ControlEResource.getResource
+		throw new UnsupportedOperationException();
+	}
+
+	public void verifyLogin() {
+		// TODO - implement ControlEResource.verifyLogin
+		throw new UnsupportedOperationException();
+	}
+
+	public void checkUserResource() {
+		// TODO - implement ControlEResource.checkUserResource
+		throw new UnsupportedOperationException();
+	}
+
+}
