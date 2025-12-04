97d770ab43572cebc8381c6137c079d14772a9a6
Álvaro de Torres Barón <163144126+AlvaroTorresUCLM@users.noreply.github.com>
2025-12-04 16:58:15 +0100

    it11 domain implementation v.0.1.4

---
+++ library-manager-client/client_systemManagement/src/main/java/org/client_systemManagement/Domain/ControlPhysicalSpace.java
@@ -0,0 +1,30 @@
+package Domain;
+
+public class ControlPhysicalSpace {
+
+	public void requestRoom() {
+		// TODO - implement ControlPhysicalSpace.requestRoom
+		throw new UnsupportedOperationException();
+	}
+
+	public void requestStudyArea() {
+		// TODO - implement ControlPhysicalSpace.requestStudyArea
+		throw new UnsupportedOperationException();
+	}
+
+	public void cancelRequest() {
+		// TODO - implement ControlPhysicalSpace.cancelRequest
+		throw new UnsupportedOperationException();
+	}
+
+	public void updateRequest() {
+		// TODO - implement ControlPhysicalSpace.updateRequest
+		throw new UnsupportedOperationException();
+	}
+
+	public void getRequestInfo() {
+		// TODO - implement ControlPhysicalSpace.getRequestInfo
+		throw new UnsupportedOperationException();
+	}
+
+}
