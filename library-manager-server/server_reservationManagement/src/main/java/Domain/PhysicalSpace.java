19edf9b43da277982ca4028ecedb000c63cefffb
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-12-04 21:23:52 +0100

    it12 Implementation v0.2.5

---
+++ library-manager-server/server_reservationManagement/src/main/java/Domain/PhysicalSpace.java
@@ -0,0 +1,16 @@
+package Domain;
+
+import java.util.Date;
+
+public class PhysicalSpace {
+
+    private int spaceID;
+    private String name;
+    private int capacity;
+    private String location;
+
+    public boolean isAvailable(Date start, Date end) {
+        // TODO - implement PhysicalSpace.isAvailable
+        throw new UnsupportedOperationException();
+    }
+}
