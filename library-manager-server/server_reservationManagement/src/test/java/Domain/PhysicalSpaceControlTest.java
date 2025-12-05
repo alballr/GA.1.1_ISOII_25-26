4ca1eb80edcedd24074e2ea78425acd2df3f2378
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-12-05 21:21:58 +0100

    it12 Implementation v0.2.7

---
+++ library-manager-server/server_reservationManagement/src/test/java/Domain/PhysicalSpaceControlTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PhysicalSpaceControlTest {
+
+    @Test
+    public void testRequestReservation() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCancelReservation() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testModifyReservation() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAvailableSpaces() {
+        fail("Not yet implemented");
+    }
+}
