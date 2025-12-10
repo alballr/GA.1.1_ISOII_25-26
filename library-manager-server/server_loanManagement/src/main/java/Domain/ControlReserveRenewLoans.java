0cc67f41e7c7995d992d34dd7d51c60bd50a7b6f
irem-batigun <Irem.Batigun1@alu.uclm.es>
2025-12-10 20:20:15 +0100

    it14 Implementation v.0.2.4

---
+++ library-manager-server/server_loanManagement/src/main/java/Domain/ControlReserveRenewLoans.java
@@ -0,0 +1,24 @@
+package Domain;
+
+public class ControlReserveRenewLoans {
+
+	/**
+	 * 
+	 * @param userId
+	 * @param itemId
+	 */
+	public boolean reserveItem(String userId, String itemId) {
+		// TODO - implement ControlReserveRenewLoans.reserveItem
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param loanId
+	 */
+	public boolean renewLoan(String loanId) {
+		// TODO - implement ControlReserveRenewLoans.renewLoan
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ library-manager-server/server_loanManagement/src/main/java/Presentation/LibraryServerInterface.java
@@ -0,0 +1,32 @@
+package Presentation;
+
+public class LibraryServerInterface {
+
+	/**
+	 * 
+	 * @param request
+	 */
+	public Response handleReserveRequest(Request request) {
+		// TODO - implement LibraryServerInterface.handleReserveRequest
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param request
+	 */
+	public Response handleRenewRequest(Request request) {
+		// TODO - implement LibraryServerInterface.handleRenewRequest
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param response
+	 */
+	public void sendResponse(Response response) {
+		// TODO - implement LibraryServerInterface.sendResponse
+		throw new UnsupportedOperationException();
+	}
+
+}
