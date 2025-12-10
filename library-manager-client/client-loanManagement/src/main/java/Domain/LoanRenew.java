543fc15b1fd89f3f2fb65a37478cba728a3beda3
luciasc04 <lucia@Lucia.localdomain>
2025-12-10 19:51:26 +0100

    it14 implementation v 0.2.4

---
+++ library-manager-client/client-loanManagement/src/main/java/Domain/LoanRenew.java
@@ -0,0 +1,11 @@
+package Domain3;
+
+public class LoanRenew {
+
+	private String loanId;
+	private Date dueDate;
+	private String renewalRemaining;
+	private String bookId;
+	private String title;
+
+}
