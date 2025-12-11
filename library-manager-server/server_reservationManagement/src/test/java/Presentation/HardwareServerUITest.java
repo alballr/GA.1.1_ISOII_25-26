d39f4efef761f7f582f187700bd41f1e00374cb7
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-12-11 20:33:54 +0100

    it13 Testing v 0.3.9

---
+++ library-manager-server/server_reservationManagement/src/test/java/Presentation/HardwareServerUITest.java
@@ -0,0 +1,31 @@
+package Presentation;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HardwareServerUITest {
+
+    @Test
+    public void testRequestHardwareLoan() {
+        // TODO - implement HardwareServerUITest.testRequestHardwareLoan
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCancelHardwareLoan() {
+        // TODO - implement HardwareServerUITest.testCancelHardwareLoan
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testModifyHardwareLoan() {
+        // TODO - implement HardwareServerUITest.testModifyHardwareLoan
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testShowAvailableHardware() {
+        // TODO - implement HardwareServerUITest.testShowAvailableHardware
+        fail("Not yet implemented");
+    }
+}
