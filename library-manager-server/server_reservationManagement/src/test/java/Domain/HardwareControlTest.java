d1f0c27f7922d2decd6ce6f73b648523f4cb7c9c
KOBRY <166229492+kobrymoataz@users.noreply.github.com>
2025-12-11 20:31:48 +0100

    it13 Implementation v 0.3.8

---
+++ library-manager-server/server_reservationManagement/src/test/java/Domain/HardwareControlTest.java
@@ -0,0 +1,31 @@
+package Domain;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HardwareControlTest {
+
+    @Test
+    public void testRequestHardwareLoan() {
+        // TODO - implement HardwareControlTest.testRequestHardwareLoan
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCancelHardwareLoan() {
+        // TODO - implement HardwareControlTest.testCancelHardwareLoan
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testModifyHardwareLoan() {
+        // TODO - implement HardwareControlTest.testModifyHardwareLoan
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAvailableHardware() {
+        // TODO - implement HardwareControlTest.testGetAvailableHardware
+        fail("Not yet implemented");
+    }
+}
